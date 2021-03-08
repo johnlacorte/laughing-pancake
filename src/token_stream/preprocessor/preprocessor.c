755d039f0e79e8e7cbcece15d872926b7f3b74dc
John LaCorte <zykryct@gmail.com>
2021-03-08 07:05:04 -0600

    Building up the preprocessor, some of the functions only return a value just to get it to compile.

--- src/token_stream/preprocessor/preprocessor.c
+++ src/token_stream/preprocessor/preprocessor.c
@@ -1,79 +1,129 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdlib.h>
+//Questioning if I even need strinng.h
+#include <string.h>
 
 #include "char_stream/char_stream.h"
 #include "preprocessor.h"
 
+#define ERROR_MSG_MAX_LENGTH 128
+
 typedef struct
 {
-    char_stream_t *stream;
-    int           status;
-    char          *status_string;
+    input_file_t  *next;
     int           line;
     int           position;
-    int           last_read;
+    char_stream_t stream;
+    char          *short_filename;
+    char          long_filename[PATH_MAX];
+} input_file_t;
+
+typedef struct
+{
+    char         *short_filename;
+    input_file_t *input_file_stack;
+    int          status;
+    int          last_read;
+    char         *error_msg;
+    char         error_msg_buffer[ERROR_MSG_MAX_LENGTH];
 } preproc_state_t;
+
+static int open_input_file(input_file_t *stack, char *filename);
+
+static int is_input_file_open(input_file *stack);
 
 preproc_t preproc_open_wat(char *filename)
 {
     preproc_state_t *state = malloc(sizeof(preproc_state_t));
     if(state != NULL)
     {
-        open_char_stream(state->stream, filename);
-        if(is_char_stream_open(state->stream))
+        if(open_input_file(state->input_file_stack, filename))
         {
-            state->status = PREPROC_OK;
-            state->status_string = "No Errors.";
-        }
+            if(is_input_file_open(state->stream))
+            {
+                state->status = PREPROC_OK;
+                state->error_msg = "No Errors.";
+            }
 
-        else
-        {
-            state->status = PREPROC_ERROR;
-            state->status_string = "Error opening file.";
+            else
+            {
+                state->status = PREPROC_ERROR;
+                //Build error msg with filename
+                state->error_msg = "Error opening file.";
+            }
+            state->last_read = ' ';
         }
-        state->line = 0;
-        state->position = 0;
-        state->last_read = ' ';
     }
 
     return (preproc_t)state;
 }
 
+static void close_input_files(input_file_t *stack);
+
 void preproc_close_wat(preproc_t pre)
 {
     preproc_state_t *state = (preproc_state_t*)pre;
-    //change to close a char_stream_t
-    close_char_stream(state->stream);
+    if(state != NULL)
+    {
+        close_input_files(state->input_file_stack);
+    }
+
+    //Possibly free'ing NULL pointer here if the caller of these functions
+    //isn't checking for NULL pointers
     free(state);
 }
 
-//I think I might use status for something else and instead return
-//0 or if last_read was negative
 int preproc_check_status(preproc_t pre)
 {
     preproc_state_t *state = (preproc_state_t*)pre;
-    return state->status;
+    if(state == NULL)
+    {
+        return PREPROC_ERROR;
+    }
+
+    else
+    {
+        return state->status;
+    }
 }
 
-//Is this supposed to return a const char* ?
 char *preproc_error_msg(preproc_t pre)
 {
     preproc_state_t *state = (preproc_state_t*)pre;
-    return state->status_string;
+    if(state == NULL)
+    {
+        return "Memory allocation for preprocessor failed. (Function was passed NULL pointer)";
+    }
+
+    else
+    {
+        return state->status_string;
+    }
 }
 
 int preproc_current_line(preproc_t pre)
 {
+    //Check for NULL or if file is even open
     preproc_state_t *state = (preproc_state_t*)pre;
     return state->line;
 }
 
 int preproc_line_position(preproc_t pre)
 {
+    //check for NULL or if file is even open
     preproc_state_t *state = (preproc_state_t*)pre;
     return state->position;
 }
+
+char *preproc_file_name(preproc_t pre)
+{
+    preproc_state_t *state = (preproc_state_t*)pre;
+    //return state->short_filename;
+    return "filename.wat";
+}
+
+static int read_7bit(input_file_t *stack);
 
 int preproc_read_char(preproc_t pre)
 {
@@ -82,34 +132,56 @@ int preproc_read_char(preproc_t pre)
     return 0;
 }
 
-void preproc_read_name(preproc_t pre, char *buffer)
+int preproc_read_extern_name(preproc_t pre)
 {
-
-}
-
-void preproc_read_extern_name(preproc_t pre, char *buffer)
-{
-
-}
-void preproc_read_data(preproc_t pre, char_buffer_t *buffer)
-{
-
-}
-
-char *preproc_read_string(preproc_t pre)
-{
-    //I'm probably going to return something other than a char*
-    //like a struct with string length and status when finished
-    preproc_state_t *state = (preproc_state_t*)pre;
     return 0;
 }
 
+int preproc_read_data(preproc_t pre)
+{
+    return 0;
+}
+
+static int preproc_dump_string(preproc_t pre, FILE *fp);
+
 //Maybe this should return a string telling what happenned
-//or the status when it finished
+//or the status when it finished of maybe a comment?
 void preproc_dump(preproc_t pre, char *filename)
 {
     preproc_state_t *state = (preproc_state_t*)pre;
     //write this function
+}
+
+static int open_input_file(input_file_t *stack, char *filename)
+{
+    return 0;
+}
+
+static int is_input_file_open(input_file *stack)
+{
+    return 0;
+}
+
+static void close_input_files(input_file_t *stack)
+{
+
+}
+
+static int read_7bit(input_file_t *stack)
+{
+    return 0;
+}
+
+static int read_utf8(input_file_t *stack)
+{
+    return 0;
+}
+
+int preproc_dump_string(preproc_t pre, FILE *fp)
+{
+    //I think I'm just going to read 
+    preproc_state_t *state = (preproc_state_t*)pre;
+    return 0;
 }
 
 /* end of file "preprocessor.c" */
--- src/token_stream/preprocessor/preprocessor.h
+++ src/token_stream/preprocessor/preprocessor.h
@@ -1,25 +1,19 @@
 #ifndef PREPOCESSOR_H
 #define PREPROCESSOR_H
 
-typedef struct
-{
-    int  length;
-    int  max;
-    int  status;
-    char *buffer;
-} char_buffer_t;
-
 //So far everything is normal
 #define PREPROC_OK 0
 
 //Normal EOF
 #define PREPROC_EOF -1
 
+#define PREPROC_END_OF_STRING -2
+
 //Error opening file, error allocating memory, etc.
-#define PREPROC_ERROR -2
+#define PREPROC_ERROR -3
 
 //Some sort of error trying to read the file
-#define PREPROC_FILE_ERROR -3
+#define PREPROC_FILE_ERROR -4
 
 //Nobody needs to know the details of the state
 typedef void* preproc_t;
@@ -39,17 +33,19 @@ int preproc_current_line(preproc_t pre);
 
 int preproc_line_position(preproc_t pre);
 
+//Maybe short file name to be used to create output file name?
+char *preproc_file_name(preproc_t pre);
+
 //Returns a 7-bit character or status to indicate why it couldn't
 int preproc_read_char(preproc_t pre);
 
-void preproc_read_name(preproc_t pre, char *buffer);
 void preproc_read_extern_name(preproc_t pre, char *buffer);
+
 void preproc_read_data(preproc_t pre, char_buffer_t *buffer);
 //Call this after reading a quotation mark and it will allocate a character
 //array from the heap, check for valid utf-8 and convert escape sequences.
 //Remember to free() this pointer when you are finished. I'm thinking of
 //dumping the stuff that was read to a file in case of an error.
-char *preproc_read_string(preproc_t pre);
 
 void preproc_dump(preproc_t pre, char *filename);
 
