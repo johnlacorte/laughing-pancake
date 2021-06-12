d8b8895815e8f796da66c0d941eb311fd97015c3
John LaCorte <zykryct@gmail.com>
2021-06-11 23:52:23 -0500

    More work to token_stream

--- src/token_stream/token_types.h
+++ src/token_stream/token_types.h
@@ -1,10 +1,12 @@
 #ifndef TOKEN_TYPES_H
 #define TOKEN_TYPES_H
 
+#include "keyword_matcher/keyword_tokens.h"
+#include "keyword_matcher/opcode_tokens.h"
+
 #define TOKEN_ERROR -2
 #define TOKEN_EOF -1
 #define TOKEN_OPEN_PAREN 0
 #define TOKEN_CLOSE_PAREN 1
-#define TOKEN_KEYWORD_MODULE 2
 
 #endif
