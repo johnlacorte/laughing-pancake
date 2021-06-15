18e9ab380b42e1a71c304f1acf41eb2f566171cb
John LaCorte <zykryct@gmail.com>
2021-06-15 12:58:05 -0500

    Made changes to this file...

--- src/token_stream/token_stream.h
+++ src/token_stream/token_stream.h
@@ -14,12 +14,12 @@ typedef void* token_stream_t;
 
 token_stream_t open_token_stream(char *filename);
 
-void free_token_stream(token_stream_t stream);
+void free_token_stream(token_stream_t token_stream);
 
-void read_token_from_stream(token_stream_t stream, token_t *token);
+void read_token_from_stream(token_stream_t token_stream, token_t *token);
 
-void read_extern_name_from_stream(token_stream_t stream, token_t *token);
+void read_extern_name_from_stream(token_stream_t token_stream, token_t *token);
 
-void read_data_from_stream(token_stream_t stream, token_t *token);
+void read_data_from_stream(token_stream_t token_stream, token_t *token);
 
 #endif
