95ba8dc0fa00e6840ac71b9386786764a648d57a
John LaCorte <zykryct@gmail.com>
2021-06-30 23:26:13 -0500

    Whoops got to change a last thing before committing.

--- src/preprocessor/utf8_encoder/utf8_encoder.h
+++ src/preprocessor/utf8_encoder/utf8_encoder.h
@@ -2,7 +2,12 @@
  * @file utf8_encoder.h
  * @brief This header file declares stuff for encoding utf8.
  *
- *
+ * To use just pass a pointer to a utf8_encoder_t to any function. First
+ * call init_utf8_encoder() then give the code point you want to encode
+ * with the encode_codepoint_to_utf8() function and read from it using
+ * is_utf8_encoder_empty() and read_next_byte_from_encoder(). If
+ * read_next_byte_from_encoder() returns ENCODER_ERROR you can get a pointer
+ * to the error message with get_utf8_encoder_error_msg().
  */
 #ifndef UTF8_ENCODER_H
 #define UTF8_ENCODER_H
@@ -14,9 +19,7 @@
 #define ENCODER_ERROR -1
 
 /**
- * @brief
- *
- *
+ * @brief utf8_encoder_t holds the state of the encoder.
  */
 typedef struct
 {
@@ -58,7 +61,7 @@ bool is_utf8_encoder_empty(utf8_encoder_t *encoder);
  * @param encoder Pointer to a utf8_encoder_t.
  * @param ch Unicode code point.
  */
-void convert_codepoint_to_utf8(utf8_encoder_t *encoder, int32_t ch);
+void encode_codepoint_to_utf8(utf8_encoder_t *encoder, int32_t ch);
 
 /**
  * @brief Read next byte of utf8 from encoder.
@@ -79,7 +82,19 @@ int  read_next_byte_from_encoder(utf8_encoder_t *encoder);
  * @param encoder Pointer to a utf8_encoder_t.
  * @return Pointer to character array with error message.
  */
-char *get_encoder_error_msg(utf8_encoder_t *encoder);
+char *get_utf8_encoder_error_msg(utf8_encoder_t *encoder);
+
+/**
+ * @brief Not implemented, returns NULL.
+ *
+ * When this is finished, it will allocate a char array, encode the string and
+ * return the pointer to the character array when finished. This just returns
+ * NULL at the moment. I think it will be needed for parser errors.
+ * @param encoder Pointer to a utf8_encoder_t.
+ * @param length The length of the string passed to the function to encode.
+ * @return NULL
+ */
+char *convert_string_to_utf8(utf8_encoder_t *encoder, size_t length, int32_t *string);
 
 #endif
 
