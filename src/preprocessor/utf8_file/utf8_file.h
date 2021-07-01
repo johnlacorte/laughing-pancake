3b5f48ce2fb79598a8ca4f778f4b6dbb5e4d21c6
John LaCorte <zykryct@gmail.com>
2021-06-30 23:37:34 -0500

    Changed so a utf8_file_t is allocated from the heap to hide a data structure capable of keeping all the lines so far read from a source file. I think this is done until I have to change the documentation to say the line thing is complete.

--- src/preprocessor/utf8_file/utf8_file.h
+++ src/preprocessor/utf8_file/utf8_file.h
@@ -23,36 +23,7 @@
 ///Status of utf8_file_t when it encounters an error.
 #define UTF8_FILE_ERROR -2
 
-///Status of utf8_file_t before a file is open or after it is closed.
-#define UTF8_FILE_CLOSED -4
-
-/**
- * @brief utf8_file_t holds all the state for reading a utf8 file.
- *
- * Can't simply push a codepoint back to the file stream so return_temp_next
- * and temp are used to check for Byte Order Marker at the start of the file
- * and to peek ahead of \r to check if it is followed by \n.
- */
-typedef struct
-{
-    FILE    *fp;
-    int32_t status;
-    bool    return_temp_next;
-    int32_t temp;
-    char    *error_msg;
-    int     line;
-    int     line_position;
-} utf8_file_t;
-
-/**
- * @brief Initializes a utf8_file_t.
- *
- * This function initializes all the elements of a utf8_file_t. Afterwards you
- * can open and close files all you want and it will properly keep track of the
- * state.
- * @param file Pointer to the utf8_file_t to initialize.
- */
-void    init_utf8_file(utf8_file_t *file);
+typedef void* utf8_file_t;
 
 /**
  * @brief Opens utf8 file for reading.
@@ -62,50 +33,47 @@ void    init_utf8_file(utf8_file_t *file);
  * doesn't. Checking the return value doesn't matter because any attempt to
  * read if a file failed to open will return UTF8_FILE_ERROR and the error
  * message will have already been set.
- * @param file Pointer to the utf8_file_t to open.
  * @param filename The filename to pass to fopen() as is.
- * @return bool. True if file opened false if it failed.
+ * @return A void pointer to the structure that keeps the state (utf8_file_t).
  */
-bool    open_utf8_file(utf8_file_t *file, char *filename);
+utf8_file_t open_utf8_file(char *filename);
 
 /**
- * @brief Closes utf8 file
+ * @brief Frees utf8 file.
  *
- * Closing a utf8_file_t closes the file stream and it can then be used to
- * read another file. Not that I am reusing any utf8_file_t in the
- * preprocessor.
- * @param file Pointer to the utf8_file_t to close.
+ * Closes any open files and free any memory that was used.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  */
-void    close_utf8_file(utf8_file_t *file);
+void free_utf8_file(utf8_file_t file);
 
 /**
  * @brief Returns status of utf8_file_t.
  *
  * The return value of this function will be UTF8_FILE_OK, UTF8_FILE_EOF,
  * UTF8_FILE_ERROR, or UTF8_FILE_CLOSED.
- * @param file Pointer to the utf8_file_t to check the status of.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  * @return A number indicating status of utf8_file_t.
  */
-int32_t get_utf8_file_status(utf8_file_t *file);
+int32_t get_utf8_file_status(utf8_file_t file);
 
 /**
  * @brief Returns a character pointer to a string of the last error.
  *
  * If there is no error this returns a pointer to the string
  * "File open, no errors."
- * @param file Pointer to the utf8_file_t to get the last error message of.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  * @return Pointer to error message.
  */
-char    *get_utf8_file_error_msg(utf8_file_t *file);
+char    *get_utf8_file_error_msg(utf8_file_t file);
 
 /**
  * @brief Returns the line the last character read is on.
  *
  * The count starts at one and increases after every newline.
- * @param file Pointer to the utf8_file_t.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  * @return The number of the current line.
  */
-int     get_utf8_file_line_number(utf8_file_t *file);
+int     get_utf8_file_line_number(utf8_file_t file);
 
 /**
  * @brief Returns the position in the line the last character read is on.
@@ -114,10 +82,10 @@ int     get_utf8_file_line_number(utf8_file_t *file);
  * after a newline and increases after each read of unicode codepoints. Using
  * pop_byte_from_utf8_file() (when testing if malformed utf8 errors are being
  * triggered for example) will lead to the count being off.
- * @param file Pointer to the utf8_file_t.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  * @return The number of codepoints read from the beginning of the line.
  */
-int     get_utf8_file_line_position(utf8_file_t *file);
+int     get_utf8_file_line_position(utf8_file_t file);
 
 /**
  * @brief returns the next byte from the file without decoding it.
@@ -128,19 +96,19 @@ int     get_utf8_file_line_position(utf8_file_t *file);
  * using this function or it will return UTF8_FILE_ERROR instead of reading
  * any bytes. This function will not change the status unless it encounters
  * an end of file.
- * @param file Pointer to the utf8_file_t to read from.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  * @return UTF8_FILE_EOF, UTF8_FILE_ERROR, or value of the byte. 
  */
-int32_t pop_byte_from_utf8_file(utf8_file_t *file);
+int32_t pop_byte_from_utf8_file(utf8_file_t file);
 
 /**
  * @brief Resets the status so you can test another error.
  *
  * Resets status, error message, file position, etc. to what they were after
  * the file successfully opened. 
- * @param file Pointer to the utf8_file_t to reset.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  */
-void    reset_utf8_file_status(utf8_file_t *file);
+void    reset_utf8_file_status(utf8_file_t file);
 
 /**
  * @brief Decodes a unicode codepoint from the file.
@@ -150,10 +118,10 @@ void    reset_utf8_file_status(utf8_file_t *file);
  * to the \n type. It stops reading if the end of file is found or some error
  * is encountered and will only return UTF8_FILE_EOF or UTF8_FILE_ERROR from
  * then on.
- * @param file Pointer to the utf8_file_t to read from.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  * @return UTF8_FILE_EOF, UTF8_FILE_ERROR, or a codepoint.
  */
-int32_t read_char_from_utf8_file(utf8_file_t *file);
+int32_t read_char_from_utf8_file(utf8_file_t file);
 
 /**
  * @brief Reads hex digits in a unicode escape and returns a codepoint.
@@ -163,10 +131,10 @@ int32_t read_char_from_utf8_file(utf8_file_t *file);
  * codepoint with the same checks that codepoints read from utf8 have to pass.
  * An end of file found in an escape will return an error along with normal
  * errors you would expect. 
- * @param file Pointer to the utf8_file_t to read from.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  * @return A codepoint or UTF8_FILE_ERROR for an error.
  */
-int32_t read_unicode_escape_from_utf8_file(utf8_file_t *file);
+int32_t read_unicode_escape_from_utf8_file(utf8_file_t file);
 
 /**
  * @brief Reads a byte in a data string. Pass the first hex digit to it.
@@ -175,11 +143,34 @@ int32_t read_unicode_escape_from_utf8_file(utf8_file_t *file);
  * digit can be passed to this function to read the second (byte escapes are
  * exactly two hex digits in WebAssembly) and it returns the value of the byte
  * or UTF8_FILE_ERROR.
- * @param file Pointer to the utf8_file_t to read from.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
  * @param first_ch The first hex digit.
  * @return The value of the byte or UTF8_FILE_ERROR.
  */
-int32_t read_byte_escape_from_utf8_file(utf8_file_t *file, int32_t first_ch);
+int32_t read_byte_escape_from_utf8_file(utf8_file_t file, int32_t first_ch);
+
+
+/**
+ * @brief Not implemented, returns zero.
+ *
+ * When this is finished, it will return the length of a line from a file. This
+ * just returns zero at the moment. I think it will be needed for parser errors.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
+ * @param line_number
+ * @return zero
+ */
+size_t get_utf8_file_line_length(utf8_file_t file, int line_number);
+
+/**
+ * @brief Not implemented, returns NULL.
+ *
+ * When this is finished, it will return the line from a file. This just returns
+ * NULL at the moment. I think it will be needed for parser errors.
+ * @param file void pointer to the structure that keeps the state(utf8_file_t).
+ * @param line_number
+ * @return NULL
+ */
+int32_t *get_utf8_file_line_string(utf8_file_t file, int line_number);
 
 #endif
 
