51273eaa9c677ffebb3c112b957480a71a2b948c
John LaCorte <zykryct@gmail.com>
2021-08-01 03:19:08 -0500

    Wrote a little bit more my time is a little constrained to finish in one session but I want the green square for the day anyway.

--- src/keyword_matcher_generator/keyword_matcher_generator.c
+++ src/keyword_matcher_generator/keyword_matcher_generator.c
@@ -8,9 +8,11 @@ struct trie_node
     trie_node *node_array[256];
 } typedef trie_node_t;
 
-int read_next_token(trie_node_t *head, FILE *input_file, FILE *constant_file);
-
-void dump_to_file(trie_node_t *head, char *matcher_file);
+int read_all_tokens(trie_node_t *head,
+                    FILE *input_file,
+                    FILE *constant_file,
+                    FILE *matcher_file
+                   );
 
 //I imagine this should take three command line arguments; file to read from,
 //constants file to write to, and keyword matcher c source file.
@@ -19,10 +21,14 @@ int main()
     trie_node_t *head = calloc(1, sizeof(trie_node_t));
     FILE *input_file = fopen("input_file", "r");
     FILE *constant_file = fopen("keyword_tokens.h", "w");
-    if(head != NULL && input_file != NULL && constant_file != NULL)
+    FILE *matcher_file = fopen("keyword_matcher.c", "w");
+    if((head != NULL) &&
+       (input_file != NULL) &&
+       (constant_file != NULL) && 
+       (matcher_file != NULL)
+      )
     {
-        while(read_next_token(head, input_file, constant_file));
-        return 0;
+        return read_all_tokens(head, input_file, constant_file, matcher_file);
     }
 
     else
@@ -31,26 +37,28 @@ int main()
     } 
 }
 
-int read_next_token_char(trie_node_t *node);
-
-int read_token_constant_name(trie_node_t *node);
-
-int read_next_token(trie_node_t *head, FILE *input_file, FILE *constant_file)
+int read_all_tokens(trie_node_t *head,
+                    FILE *input_file,
+                    FILE *constant_file,
+                    FILE *matcher_file
+                   )
 {
+    int ch = fgetc(input_file);
+    while(ch != EOF)
+    {
+        //push back ch
+        if(!read_token())
+        {
+
+        }
+
+        ch = fgetc(input_file);
+    }
+
     return 0;
 }
 
-void dump_to_file(trie_node_t *head, char *matcher_file)
+void dump_to_file(trie_node_t *head, FILE *matcher_file)
 {
 
-}
-
-int read_next_token_char(trie_node_t *node)
-{
-    return 0;
-}
-
-int read_token_constant_name(trie_node_t *node)
-{
-    return 0;
 }
