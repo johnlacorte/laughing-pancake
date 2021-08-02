61226ba7d22cd6b9ca46c4e89ee840af69849763
John LaCorte <zykryct@gmail.com>
2021-08-02 00:50:54 -0500

    Added a couple structs and functions

--- src/keyword_matcher_generator/keyword_matcher_generator.c
+++ src/keyword_matcher_generator/keyword_matcher_generator.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct constant_node
+{
+    char *constant_string;
+    struct constant_node *next;
+} typedef constant_node_t;
+
+typedef struct
+{
+    constant_node_t *head;
+    constant_node_t *tail;
+} constants_list_t;
+
 struct trie_node
 {
     char *token_constant_name;
@@ -18,28 +30,42 @@ int read_all_tokens(trie_node_t *head,
 //constants file to write to, and keyword matcher c source file.
 int main()
 {
-    trie_node_t *head = calloc(1, sizeof(trie_node_t));
+    trie_node_t *trie_head = calloc(1, sizeof(trie_node_t));
+    constants_list_t *constants_list = calloc(1, sizeof(constant_list_t));
     FILE *input_file = fopen("input_file", "r");
-    FILE *constant_file = fopen("keyword_tokens.h", "w");
+    FILE *constants_file = fopen("keyword_tokens.h", "w");
     FILE *matcher_file = fopen("keyword_matcher.c", "w");
-    if((head != NULL) &&
+    if((trie_head != NULL) &&
+       (constants_list != NULL) &&
        (input_file != NULL) &&
        (constant_file != NULL) && 
        (matcher_file != NULL)
       )
     {
-        return read_all_tokens(head, input_file, constant_file, matcher_file);
+        return read_all_tokens(trie_head,
+                               constants_list,
+                               input_file,
+                               constant_file,
+                               matcher_file);
     }
 
     else
     {
+        //print to stderr
         return 1;
     } 
 }
 
-int read_all_tokens(trie_node_t *head,
+int read_token(FILE *input_file, trie_node_t *trie_head, constant_list_t *constants_list);
+
+void dump_trie_to_file(trie_node_t *trie_head, FILE *matcher_file);
+
+void dump_constants_to_file(constants_list_t *constants_list, FILE *constants_file);
+
+int read_all_tokens(trie_node_t *trie_head,
+                    constants_list *constants_list
                     FILE *input_file,
-                    FILE *constant_file,
+                    FILE *constants_file,
                     FILE *matcher_file
                    )
 {
@@ -47,9 +73,9 @@ int read_all_tokens(trie_node_t *head,
     while(ch != EOF)
     {
         //push back ch
-        if(!read_token())
+        if(!read_token(input_file, trie_head, constants_list))
         {
-
+            return 1;
         }
 
         ch = fgetc(input_file);
@@ -58,7 +84,17 @@ int read_all_tokens(trie_node_t *head,
     return 0;
 }
 
-void dump_to_file(trie_node_t *head, FILE *matcher_file)
+int read_token(FILE *input_file, trie_node_t *trie_head, constant_list_t *constants_list)
+{
+
+}
+
+void dump_trie_to_file(trie_node_t *trie_head, FILE *matcher_file)
+{
+
+}
+
+void dump_constants_to_file(constant_node_t *constants_head, FILE *constants_file)
 {
 
 }
