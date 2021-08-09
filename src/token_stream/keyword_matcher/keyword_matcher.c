5d8f61712039dc33ef7b03edeb9b344d1a8997f8
John LaCorte <zykryct@gmail.com>
2021-08-09 04:43:46 -0500

    Replaced the files with ones generated by a program

--- src/token_stream/keyword_matcher/keyword_matcher.c
+++ src/token_stream/keyword_matcher/keyword_matcher.c
@@ -1,49 +1,69 @@
+#include "token_types.h"
 #include "keyword_tokens.h"
-#include "opcode_tokens.h"
 
 int keyword_matcher(char *token_buffer)
 {
   switch(token_buffer[0])
   {
-    case '(':
+    case 34:
     switch(token_buffer[1])
     {
-        case '\0':
-        return TOKEN_OPEN_PAREN;
+      case 0:
+      return TOKEN_DOUBLE_QUOTE;
 
-        default:
-        return TOKEN_NO_MATCH;
+      default:
+      return TOKEN_NO_MATCH;
     }
 
-    case ')':
+    case 40:
     switch(token_buffer[1])
     {
-        case '\0':
-        return TOKEN_CLOSE_PAREN;
+      case 0:
+      return TOKEN_OPEN_PAREN;
 
-        default:
-        return TOKEN_NO_MATCH;
+      default:
+      return TOKEN_NO_MATCH;
     }
 
-    case 'm':
+    case 41:
     switch(token_buffer[1])
     {
-      case 'o':
+      case 0:
+      return TOKEN_CLOSE_PAREN;
+
+      default:
+      return TOKEN_NO_MATCH;
+    }
+
+    case 91:
+    switch(token_buffer[1])
+    {
+      case 0:
+      return TOKEN_OPEN_SQUARE_BRACKET;
+
+      default:
+      return TOKEN_NO_MATCH;
+    }
+
+    case 109:
+    switch(token_buffer[1])
+    {
+      case 111:
       switch(token_buffer[2])
       {
-        case 'd':
+        case 100:
         switch(token_buffer[3])
         {
-          case 'u':
+          case 117:
           switch(token_buffer[4])
           {
-            case 'l':
+            case 108:
             switch(token_buffer[5])
             {
-              case 'e':
+              case 101:
               switch(token_buffer[6])
               {
-                case '\0':
+                case 0:
                 return TOKEN_KEYWORD_MODULE;
 
                 default:
@@ -74,3 +94,4 @@ int keyword_matcher(char *token_buffer)
     return TOKEN_NO_MATCH;
   }
 }
+
