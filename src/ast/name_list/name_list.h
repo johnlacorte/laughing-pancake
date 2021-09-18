d3cc469acc6a99e47aed95d0ecdd5c2c7c896960
John LaCorte <zykryct@gmail.com>
2021-09-18 03:18:05 -0500

    Filled in details

--- src/ast/name_list/name_list.h
+++ src/ast/name_list/name_list.h
@@ -15,4 +15,10 @@ typedef struct
     name_t *head;
 } name_list_t;
 
+
+void init_name_list(name_list_t *name_list);
+
+bool add_name_to_name_list(name_list_t *name_list);
+
+int get_name_index(name_list_t *name_list);
 #endif
