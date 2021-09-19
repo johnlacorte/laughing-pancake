79c4ed77e0e8517c448194d8b3f118d733b996e0
John LaCorte <zykryct@gmail.com>
2021-09-19 16:48:54 -0500

    Finished writing name_map.h

--- src/ast/name_map/name_map.h
+++ src/ast/name_map/name_map.h
@@ -1,10 +1,10 @@
-#ifndef NAME_LIST_H
-#define NAME_LIST_H
+#ifndef NAME_MAP_H
+#define NAME_MAP_H
 
 typedef struct name
 {
-    /* data */
     char *name_string;
+    int index;
     struct name *next;
 } name_t;
 
@@ -13,12 +13,15 @@ typedef struct
 {
     int count;
     name_t *head;
-} name_list_t;
+} name_map_t;
 
 
-void init_name_list(name_list_t *name_list);
+void init_name_map(name_map_t *name_map);
 
-bool add_name_to_name_list(name_list_t *name_list);
+bool add_name_to_name_map(name_map_t *name_map, char *name, int index);
 
-int get_name_index(name_list_t *name_list);
+int get_name_index(name_map_t *name_map, char *name);
+
 #endif
+
+/*** end of file "name_map.h" ***/
