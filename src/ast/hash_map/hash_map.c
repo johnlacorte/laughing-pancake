a896c6afc64edd2998ce0b300328853e51407d65
John LaCorte <zykryct@gmail.com>
2021-09-21 10:45:28 -0500

    Created file hash_map.c

---
+++ src/ast/hash_map/hash_map.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include "hash_map.h"
+
+bool init_hash_map(hash_map_t *hash_map)
+{
+    return false;
+}
+
+char *add_name_to_hash_map(hash_map_t *hash_map, char *name)
+{
+    return NULL;
+}
+
+/*** end of file "hash_map.c" ***/
