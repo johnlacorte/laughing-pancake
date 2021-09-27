cb3a6bc9c0332b489cb5aa0640eda5093f9838be
John LaCorte <zykryct@gmail.com>
2021-09-27 07:14:24 -0500

    Added function add_name_to_hash_map()

--- src/ast/hash_map/hash_map.c
+++ src/ast/hash_map/hash_map.c
@@ -1,10 +1,11 @@
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "hash_map.h"
 
 typedef struct name
 {
-    /* data */
     char *name;
     struct name *next;
 } name_t;
@@ -12,18 +13,19 @@ typedef struct name
 typedef struct
 {
     char **slots;
-    int number_of_names;
-    int number_of_slots;
+    unsigned int number_of_names;
+    unsigned int number_of_slots;
     name_t *name_list_head;
     name_t *name_list_tail;
 } hash_map_state_t;
 
-hash_map_t new_hash_map(int initial_size)
+hash_map_t new_hash_map(unsigned int initial_size)
 {
     hash_map_state_t *new = malloc(sizeof(hash_map_state_t));
     if(new != NULL)
     {
-        new->slots = malloc(sizeof(char**) * initial_size);
+        //slots needs to be set to NULL
+        new->slots = calloc(initial_size, sizeof(char**));
         if(new->slots != NULL)
         {
             new->number_of_names = 0;
@@ -56,14 +58,103 @@ void free_hash_map(hash_map_t hash_map)
     free(state);
 }
 
+static unsigned int hash_function(char *name, unsigned int number_of_slots);
+
+static char *add_name_to_name_list(name_t *name_list_head, char *name);
+
 char *add_name_to_hash_map(hash_map_t hash_map, char *name)
 {
-    return NULL;
+    //check for null pointer
+    if(hash_map != NULL)
+    {
+        hash_map_state_t *state = (hash_map_state_t*)hash_map;
+        unsigned int slot_number = hash_function(name, state->number_of_slots);
+        //search to end of array
+        for(unsigned int i = slot_number; i < state->number_of_slots; i++)
+        {
+            if(state->slots[i] == NULL)
+            {
+                //add to name list and slot[i]
+                state->slots[i] = add_name_to_name_list(state->name_list_head,
+                                                        name);
+                if(state->slots[i] == NULL)
+                {
+                    //free hash map, assume an error message was printed to stderr
+                    free_hash_map(hash_map);
+                    return NULL;
+                }
+
+                else
+                {
+                    return state->slots[i];
+                }
+            }
+
+            else
+            {
+                //compare strings
+                if(strcmp(name, state->slots[i]) == 0)
+                {
+                    return state->slots[i];
+                }
+            }
+        }
+
+        //search staring from beginning of array
+        for(unsigned int i = 0; i < slot_number; i++)
+        {
+            if(state->slots[i] == NULL)
+            {
+                //add to name list and slot[i]
+                state->slots[i] = add_name_to_name_list(state->name_list_head,
+                                                        name);
+                if(state->slots[i] == NULL)
+                {
+                    //free hash map, assume an error message was printed to stderr
+                    free_hash_map(hash_map);
+                    return NULL;
+                }
+
+                else
+                {
+                    return state->slots[i];
+                }
+            }
+
+            else
+            {
+                //compare strings
+                if(strcmp(name, state->slots[i]) == 0)
+                {
+                    return state->slots[i];
+                }
+            }
+        }
+
+        //For some reason all slots are full print error message to stderr
+        return NULL;
+    }
+
+    else
+    {
+        //failed to allocate memory for hash map print error message to stderr
+        return NULL;
+    }
 }
 
 static void free_name_list(name_t *name_list_head)
 {
 
+}
+
+static unsigned int hash_function(char *name, unsigned int number_of_slots)
+{
+    return 0;
+}
+
+static char *add_name_to_name_list(name_t *name_list_head, char *name)
+{
+    return NULL;
 }
 
 /*** end of file "hash_map.c" ***/
